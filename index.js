9fa8372baa0494e1ee940ca6fd70c6d8e62ed4ae
dunbarj <dunbarj>
2017-12-07 15:30:45 -0500

    Fixed tipping

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ var reply_report_threshold = 3;
 
 //===== Express =====//
 var app = express();
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+app.use(bodyParser.json({limit: '100mb'}));
 
 function guid() {
     function _p8(s) {
@@ -187,10 +187,10 @@ app.post('/user/:uid/tip', function(request, response) {
     exchangeCurrency(user_from, user_to, amount, function (result) {
         if (result < 0) {
             console.log("Currency exchange from user " + user_from + " to user " + user_to + " has failed!");
-            response.sendStatus(400);
+            response.send("failed");
             return;
         }
-        response.sendStatus(200);
+        response.send("success");
     });
 });
 
